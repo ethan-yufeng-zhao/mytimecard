3020bbfa457d52c481a594dc6acc6149d1f77ab6
ethan.zhao <ethan.zhao@jfab.aosmd.com>
2025-08-30 13:44:23 -0700

    remove useless files

--- sql_backup/backup.bat
+++
@@ -1,7 +0,0 @@
-REM
-REM	to use this just enter the sql files in this directory to backup
-REM	EXAMPLE:
-REM	backup.bat monthly.sql
-REM
-REM
-"C:\Program Files (x86)\MySQL\MySQL Server 5.5\bin\mysqldump.exe" --opt --host jfabweb3 --user root --password=t0rtur3d tcs > C:\inetpub\wwwroot\Training_Cert_System\sql_backup\%1
--- sql_backup/restore.bat
+++
@@ -1,7 +0,0 @@
-REM
-REM	to use this just enter the sql files in this directory to backup
-REM	EXAMPLE:
-REM	restore.bat monthly.sql
-REM
-REM
-"C:\Program Files\MySQL\MySQL Server 5.5\bin\mysql.exe" --host hilweb5 --user root --password=t0rtur3d wordpress < C:\MySQL_Backup\%1
