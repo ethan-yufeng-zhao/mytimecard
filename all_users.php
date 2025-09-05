80c6fe49f62a9a2200a74064154677ab160d1987
ethan.zhao <ethan.zhao@jfab.aosmd.com>
2025-09-05 09:41:44 -0700

    remove useless code

--- all_users.php
+++ all_users.php
@@ -66,9 +66,9 @@
 		echo("Email");
 		echo("</th>");
 
-		echo("<th>");
-		echo("Cert Count");
-		echo("</th>");
+//		echo("<th>");
+//		echo("Cert Count");
+//		echo("</th>");
 
 		echo("<th>");
 		echo("Supervisor");
@@ -117,11 +117,11 @@
 			echo('<a href="mailto:'.$value['user_email'].'">'.$value['user_email'].'</a>');
 			echo("</td>\n");
 
-			echo('<td>');
-            if($value['certcount'] > 0){
-                echo($value['certcount']);
-            }
-			echo("</td>\n");
+//			echo('<td>');
+//            if($value['certcount'] > 0){
+//                echo($value['certcount']);
+//            }
+//			echo("</td>\n");
 
 			echo('<td>');
             if($value['user_supervisor_id']) {
