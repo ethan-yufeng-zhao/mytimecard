dd53c98f4b25c426541863c31857fd4d5a168150
ethan.zhao <ethan.zhao@jfab.aosmd.com>
2025-09-05 09:42:03 -0700

    remove useless code

--- header.php
+++ header.php
@@ -298,7 +298,6 @@
                         echo('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin <b class="caret"></b></a>');
                         echo('<ul class="dropdown-menu">');
                         echo('<li><a href="'.$mybaseurl.'/all_users.php">All Users</a></li>');
-                        echo('<li><a href="'.$mybaseurl.'/all_certs.php">All Certifications</a></li>');
                         echo('<li><a href="'.$mybaseurl.'/all_users_certs.php">All Granted Certifications Report</a></li>');
                         echo('<li><a href="'.$mybaseurl.'/all_templates.php">All Templates</a></li>');
                         echo('<li><a href="'.$mybaseurl.'/all_tools.php">All Tools</a></li>');
