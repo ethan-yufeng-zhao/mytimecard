b6c65f1030e88a8e98f388fe43e892319943d5dd
ethan.zhao <ethan.zhao@jfab.aosmd.com>
2025-09-16 13:38:50 -0700

    add user role api

---
+++ JSON/JSON_user_role.php
@@ -0,0 +1,43 @@
+<?php
+require_once('..'.DIRECTORY_SEPARATOR.'base.php');
+require_once('..'.DIRECTORY_SEPARATOR.'DB'.DIRECTORY_SEPARATOR.'db.php');
+
+$current_timestamp = time();
+
+$arr = array();
+
+if(isset($_GET['uid']) && strlen($_GET['uid']) > 0) {
+    $user_id     = $_GET['uid'];
+} else {
+    $user_id     = $_GET['user_id'] ?? '';
+}
+
+if (empty($user_id)) {
+    $arr['error'] = 'Need user id';
+    header('Content-Type: application/json');
+    echo(json_encode($arr));
+    return;
+}
+
+$db_pdo = db_connect();
+
+$querystring_team_users = "SELECT count(*) FROM hr.employee WHERE manager_samaccountname = '".$user_id."'";
+$db_arr_team_users = db_query($db_pdo, $querystring_team_users);
+if ($db_arr_team_users && $db_arr_team_users[0]['count'] > 0) {
+    $arr[$user_id] = 'supervisor';
+} else {
+    $arr[$user_id] = '';
+}
+
+$querystring_admin = "SELECT role FROM hr.timecard_admin WHERE username = '".$user_id."'";
+$db_arr_admin = db_query($db_pdo, $querystring_admin);
+if ($db_arr_admin) {
+    $arr[$user_id] = $db_arr_admin[0]['role'] ?? '';
+} else {
+    $arr[$user_id] = '';
+}
+
+$db_pdo = null;
+
+header('Content-Type: application/json');
+echo(json_encode($arr));
